f5afb129da586e184612f91ec999a56b073fd34c
JeffBcyc <41636467+JeffBcyc@users.noreply.github.com>
2020-07-09 22:09:52 -0400

    0.1

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+
+    [Range (1f, 20f)][SerializeField] float gridSize = 10f;
+
+    TextMesh _cubePosition;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 snapPos;
+        snapPos.x = Mathf.Round(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.Round(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        _cubePosition = GetComponentInChildren<TextMesh>();
+        _cubePosition.text = snapPos.x + " , " + snapPos.z;
+
+
+    }
+}
