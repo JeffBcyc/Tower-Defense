20e1b078583250e0c0ea76efc0b054c30cd7007f
JeffBcyc <41636467+JeffBcyc@users.noreply.github.com>
2020-07-14 17:20:42 -0400

    added health system

--- Assets/EnemyPath.cs
+++ Assets/EnemyPath.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -8,9 +9,15 @@ public class EnemyPath : MonoBehaviour
 
     List<Waypoint> travel = new List<Waypoint>();
 
+    [SerializeField] GameObject damageText;
+
+    [SerializeField] int EnemyHP = 100;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
         travel = pathfinder.GetPath();
         StartCoroutine(EnemyPatrolPath());
@@ -24,7 +31,7 @@ public class EnemyPath : MonoBehaviour
         foreach (Waypoint waypoint in travel)
         {
             
-            transform.position = waypoint.transform.position;
+            transform.position = waypoint.transform.position + new Vector3(0,10,0);
             yield return new WaitForSeconds(0.5f);
             // ode will stop executing after the first round here
             // then it goes back to the start method to execute whats left
@@ -36,9 +43,43 @@ public class EnemyPath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    private void OnParticleCollision(GameObject other)
+    {
+        int damageTaken = UnityEngine.Random.Range(0, 10);
+        print("Hit by Particles");
+        createDamageText(damageTaken);
+        print("Dmg shown");
+        EnemyHP -= damageTaken;
+        if (EnemyHP < 0) { Destroy(gameObject); }
+        Destroy(damageText, 1.0f);
+
+    }
+
+    private void createDamageText(int damage)
+    {
+        damageText = Instantiate(damageText, transform.position + new Vector3(0,10f,0), Quaternion.identity);
+        damageText.name = Time.time.ToString();
+        
+        Rigidbody textRigid = damageText.GetComponent<Rigidbody>();
+        Vector3 initialSpeed = new Vector3(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(0, 20), UnityEngine.Random.Range(-10, 10));
+        textRigid.velocity =  initialSpeed;
+        damageText.transform.parent = gameObject.transform;
+        TextMesh damageTextMesh = damageText.GetComponent<TextMesh>();
+
+        if (damage == 0) 
+        {
+            damageTextMesh.text = "Missed!";
+        } 
+        else
+        {
+            damageTextMesh.text = damage.ToString();
+        }
+
+
+    }
 
 
 }
---
+++ Assets/Tower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField] Transform objectToPan = null;
+    [SerializeField] Transform targetToLook = null;
+
+    private void Awake()
+    {
+        Vector3 adjustedHeight = new Vector3(0, 10, 0);
+        targetToLook.position += adjustedHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StareAtEnemy();
+    }
+
+    private void StareAtEnemy()
+    {
+        objectToPan.LookAt(targetToLook);
+    }
+}
